226baf1681c469d15a37a247cac6787534bad742
paritav <parita.vara@gmail.com>
2025-11-08 23:12:07 +0700

    Delete Home.py

--- Home.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-
-st.set_page_config(
-    page_title="Data Visualization Project",
-    page_icon="📊",
-)
-
-st.title("📊 Welcome to the Data Visualization Project")
-st.sidebar.success("Select a page above to view the analysis.")
