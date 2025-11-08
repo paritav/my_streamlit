dbb7cfb68e3fb9bee299c3e49c34227196c17a4c
paritav <parita.vara@gmail.com>
2025-11-08 22:53:10 +0700

    Add files via upload

---
+++ Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title="Data Visualization Project",
+    page_icon="📊",
+)
+
+st.title("📊 Welcome to the Data Visualization Project")
+st.sidebar.success("Select a page above to view the analysis.")
